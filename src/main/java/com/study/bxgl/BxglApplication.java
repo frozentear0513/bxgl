276c4f463aee25eab4ef9ec3c9819ff880c3d1a3
wangwu <750291040@qq.com>
2021-02-18 13:06:53 +0800

    提交springboot初始远源码

---
+++ src/main/java/com/study/bxgl/BxglApplication.java
@@ -0,0 +1,13 @@
+package com.study.bxgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BxglApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BxglApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/bxgl/BxglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.bxgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BxglApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
